d01d60851626087c169b2372e0e1be1816e0fa1d
Gerald Ruiz <gruizcavero@uh.edu>
2022-07-31 15:36:32 -0500

    vars modified

--- network-variables.tf
+++ network-variables.tf
@@ -6,10 +6,12 @@ variable "location" {
 
 variable "network-vnet-cidr" {
   type        = string
+  default     = "172.16.0.0/16"
   description = "The CIDR for the network VNET"
 }
 
 variable "vm-subnet-cidr" {
   type        = string
+  default     = "172.16.0.0/24"
   description = "The CIDR for the vm subnet"
 }
